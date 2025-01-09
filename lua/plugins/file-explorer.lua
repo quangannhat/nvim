4538a8f69bb0dd322fff5b302a2cdf1bd175ca71
quangan <quangannhat.dev@gmail.com>
2025-01-09 20:37:07 +0700

    feat: add icons for oil, some harpoon keymaps

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -1,5 +1,11 @@
 return {
   "stevearc/oil.nvim",
+  dependencies = {
+    {
+      "echasnovski/mini.icons",
+      opts = {},
+    },
+  },
   config = function()
     require("oil").setup()
 
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,14 +5,23 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local harpoon = require("harpoon")
-
+      local prefix_key = "<C-h>"
       harpoon:setup()
-      vim.keymap.set("n", "<leader>a", function()
+      vim.keymap.set("n", prefix_key .. "a", function()
         harpoon:list():add()
       end)
-      vim.keymap.set("n", "<C-e>", function()
+      vim.keymap.set("n", prefix_key .. "o", function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end)
+
+      vim.keymap.set("n", prefix_key .. "1", function()
+        harpoon:list():select(1)
+      end)
+      for i = 1, 5 do
+        vim.keymap.set("n", prefix_key .. i, function()
+          harpoon:list():select(i)
+        end)
+      end
     end,
   },
 }
