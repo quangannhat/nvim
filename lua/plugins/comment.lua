29a0b5fa2193b1a16858aecfe23bb6cb48cfd723
quangan <quangannhat.dev@gmail.com>
2025-03-20 21:02:36 +0700

    feat: added commend for tsx

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,13 @@
 return {
   "numToStr/Comment.nvim",
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
   opts = {},
   config = function()
-    require("Comment").setup()
+    local tsx_comment = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+    require("Comment").setup({
+      pre_hook = tsx_comment,
+    })
   end,
 }
