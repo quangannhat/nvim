615ec81ffd90a52fedc15f56caf025eab0c6a0c3
quangan <quangannhat.dev@gmail.com>
2025-03-20 13:13:11 +0700

    feat: added snacks for file renaming using oil

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/snacks.nvim",
+  config = function()
+    local snacks = require("snacks")
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "OilActionsPost",
+      callback = function(event)
+        if event.data.actions.type == "move" then
+          snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+        end
+      end,
+    })
+  end,
+}
